59c86253603713305ea63d3e69c069df52f0b74e
Vladimir Volosnikov <volosnikovvladimir69@gmail.com>
2025-01-13 21:42:39 +0500

    Create the func lifetime to set the behavior before launching and after the application is shut down; create the func create_app to build the application.

---
+++ src/main.py
@@ -0,0 +1,54 @@
+"""A module for building and launching an application."""
+
+import logging.config
+from contextlib import asynccontextmanager
+
+from fastapi import FastAPI
+from fastapi.staticfiles import StaticFiles
+
+from .config.app_config import Config
+from .config.log_config import LOG_CONFIG
+from .db.database import engine, Base
+
+config = Config()
+logging.config.dictConfig(LOG_CONFIG)
+logger = logging.getLogger("main_logger")
+
+
+@asynccontextmanager
+async def lifespan(app_: FastAPI):
+    """
+    Add behavior before launching and after shutting down the app.
+
+    The function adds data lifting before startup
+    (as well as pre-reset data in debug mode).
+
+    :param app_: FastAPI app.
+    """
+    logger.info("Start up.")
+
+    async with engine.begin() as conn:
+        if config.debug:
+            logger.debug("Debug mode.")
+            logger.warning("Drop db.")
+            await conn.run_sync(Base.metadata.drop_all)
+
+        await conn.run_sync(Base.metadata.create_all)
+
+    yield
+
+    logger.info("Shut down.")
+    await engine.dispose()
+
+
+def create_app() -> FastAPI:
+    """Configure and create the FastAPI app."""
+    logger.info("Creating FastAPI app...")
+    app_ = FastAPI(lifespan=lifespan)
+
+    app_.mount("/", StaticFiles(directory="app/static"))
+
+    return app_
+
+
+app: FastAPI = create_app()
