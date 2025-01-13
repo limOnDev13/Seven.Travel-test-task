d64bfcdd1d4d3b4371dcc63182a038895ea20a9e
Vladimir Volosnikov <volosnikovvladimir69@gmail.com>
2025-01-13 22:10:52 +0500

    Create the ORM model Task; add the method __repr__;

---
+++ src/db/models.py
@@ -0,0 +1,21 @@
+"""The module responsible for model descriptions in the database."""
+
+from sqlalchemy import String, Text, Integer
+from sqlalchemy.orm import Mapped, mapped_column
+
+from .database import Base
+
+
+class Task(Base):
+    """ORM representation of a table in which task records will be stored"""
+
+    __tablename__ = "task"
+
+    id: Mapped[int] = mapped_column(Integer, primary_key=True, autoincrement=True)
+    title: Mapped[str] = mapped_column(String, nullable=False)
+    description: Mapped[str] = mapped_column(Text, nullable=False)
+    status: Mapped[str] = mapped_column(String(15), nullable=False)
+
+    def __repr__(self) -> str:
+        """Return the string representation of the object."""
+        return f"{self.title} ({self.id}), status: {self.status}"
