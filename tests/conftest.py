bd3d9c3467c2328283729e3bbdc18f0a473c13f5
Vladimir Volosnikov <volosnikovvladimir69@gmail.com>
2025-01-14 14:53:13 +0500

    Add fixtures dependency_session, test_app, client

--- tests/conftest.py
+++ tests/conftest.py
@@ -2,15 +2,18 @@
 
 import random
 from string import ascii_letters
-from typing import AsyncGenerator, Generator, List
+from typing import AsyncGenerator, Callable, Generator, List
 
 import pytest
 import pytest_asyncio
+from fastapi import FastAPI, Request
+from httpx import ASGITransport, AsyncClient
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from src.db.database import Session, engine
 from src.db.models import Base
 from src.db.repositories import TaskRepository
+from src.main import create_app
 from src.schemas.schemas import STATUSES, TaskInSchema
 
 
@@ -76,3 +79,33 @@ def many_task_in() -> Generator[List[TaskInSchema], None, None]:
         )
         for _ in range(random.randint(10, 100))
     ]
+
+
+@pytest_asyncio.fixture(scope="function")
+async def dependency_session(session: AsyncSession) -> AsyncGenerator[Callable, None]:
+    """Start a test database session."""
+
+    async def overrides_get_session(request: Request):
+        request.state.session = session
+        yield
+
+    yield overrides_get_session
+    await session.close()
+
+
+@pytest.fixture(scope="function")
+def test_app(dependency_session: Callable) -> Generator[FastAPI, None, None]:
+    """Create a test_app with overridden dependencies."""
+    _app: FastAPI = create_app()
+    _app.dependency_overrides[dependency_session] = dependency_session
+    yield _app
+    _app.dependency_overrides.clear()
+
+
+@pytest_asyncio.fixture(scope="function")
+async def client(test_app: FastAPI) -> AsyncGenerator[AsyncClient, None]:
+    """Create a http client."""
+    async with AsyncClient(
+        transport=ASGITransport(app=test_app), base_url="http://localhost:8000"
+    ) as client:
+        yield client
